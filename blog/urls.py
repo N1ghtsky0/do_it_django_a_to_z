9e74103a0db7baeaafdf6cabf0fccf97a60bacc2
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-11-25 17:10:07 +0900

    CBV로 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    # path('', views.index),
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 # Create your views here.
 
+# CBV로 구현
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/post_list.html'
+    ordering = '-pk'
+
+"""
+# FBV로 구현
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
-        "blog/index.html",
+        "blog/post_list.html",
         {
             'posts': posts,
         }
     )
-
+"""
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
@@ -24,3 +33,4 @@ def single_post_page(request, pk):
             'post': post
         }
     )
+
