b2eeb585523cab7fbfaf856c9f49bf1d7ee7f757
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-11-28 19:16:00 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view())
 ]
