7572a5b48ca563824a5f6dc36917be3f99a8da9c
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-11-28 13:59:32 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/tests.py
+++ blog/tests.py
@@ -14,6 +14,8 @@ class TestView(TestCase):
                                                    password='somepassword')
         self.user_obama = User.objects.create_user(username='obama',
                                                    password='somepassword')
+        self.user_obama.is_staff = True
+        self.user_obama.save()
 
         self.category_programming = Category.objects.create(name='programming',
                                                             slug='programming')
@@ -192,9 +194,13 @@ class TestView(TestCase):
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
 
-        # 로그인 진행
+        # staff가 아닌 계정으로 로그인
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
+        # 로그인 진행
+        self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -214,4 +220,4 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 4)
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, 'Post Form 만들기')
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
