24153d18c8dd7b2a155e2be7b43fd9f3d376a199
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-11-29 16:20:03 +0900

    댓글 폼으로 포스트 상세 페이지에서 댓글 작성하기

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
-    path('update_post/<int:pk>/', views.PostUpdate.as_view())
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
 from .models import Post, Category, Tag
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 
@@ -27,6 +29,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
 
         return context
 
@@ -141,3 +144,20 @@ def tag_page(request, slug):
             'categories': Category.objects.all(),
         }
     )
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
