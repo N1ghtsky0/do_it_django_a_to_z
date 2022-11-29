bab958c581846beb952efb98570bdaf3ab57a4f9
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-11-29 21:49:04 +0900

    댓글 삭제 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
 ]
--- blog/views.py
+++ blog/views.py
@@ -173,3 +173,13 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
