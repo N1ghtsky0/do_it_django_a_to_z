f2178a1f0360b49de8cbc7c51c216096ad3ab13b
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-12-02 21:40:20 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -64,6 +64,11 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1353/aec389275a4deefd/svg/{self.author.email}'
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
 def landing(request):
+    recent_post = Post.objects.order_by('-pk')[:3]
     return render(
         request,
-        'single_pages/landing.html'
+        'single_pages/landing.html',
+        {
+            'recent_posts': recent_post,
+        }
     )
 
 def about_me(request):
